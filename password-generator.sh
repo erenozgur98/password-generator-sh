fb43c8caf6e97c802e0df0c9e7161dedaaaa632c
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-02 20:05:30 -0400

    added the code, the password generator now works

--- password-generator.sh
+++ password-generator.sh
@@ -1,1 +1,15 @@
 #! /usr/bin/bash
+
+# Simple Password Generator
+
+echo "This is a simple password generator"
+
+echo "Please enter the length of the password: "
+read PASS_LENGTH
+
+# simple for loop
+
+for p in $(seq 1);
+    do
+        openssl rand -base64 48 | cut -c1-$PASS_LENGTH #cutting from column1 or first letter all the way to length of the password
+done
