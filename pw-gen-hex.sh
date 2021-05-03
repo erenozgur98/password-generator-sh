4535433f473c83c1104ff54338138b7dcd4e2084
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-02 20:14:09 -0400

    added the content insdie the pw-gen-hex

--- pw-gen-hex.sh
+++ pw-gen-hex.sh
@@ -1,3 +1,12 @@
 #! /usr/bin/bash
 
 # This is the same pw generator but with using hexedecimals
+
+echo "This is a simple password generator with hexedecimals"
+echo "Please enter the length of the password: "
+read PASS_LENGTH
+
+for p in $(seq 1 5);
+    do
+        openssl rand -hex 48 | cut -c1-$PASS_LENGTH
+done
